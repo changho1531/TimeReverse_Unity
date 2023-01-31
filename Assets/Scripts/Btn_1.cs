e2614e64758e57212ed69d62334baceba34f895e
changho1531 <98195821+changho1531@users.noreply.github.com>
2023-01-31 23:35:17 +0900

    메뉴창 UI 메커니즘 구현완료

--- Assets/Scripts/Btn_1.cs
+++ Assets/Scripts/Btn_1.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Btn_1 : MonoBehaviour
 {
-    public GameObject subMenuSet;
-    public GameObject optionSet;
-    public GameObject menuSet;
+    public GameObject subMenuSet;   // 내부 메뉴창
+    public GameObject optionSet;   // 옵션창
+    public GameObject menuSet;   // 메뉴 창
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +17,36 @@ public class Btn_1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    Debug.Log("눌렀어");
+        //    if (menuSet.activeSelf)
+        //    {
+        //        menuSet.SetActive(false);
+        //    }
+        //    else
+        //    {
+        //        menuSet.SetActive(true);
+        //    }
+        //}
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("눌렀어");
             if (menuSet.activeSelf)
             {
                 menuSet.SetActive(false);
             }
-            else
+            else if(!menuSet.activeSelf)
+            {
+                if(optionSet.activeSelf) 
+                {
+                    optionSet.SetActive(false);
+                }
                 menuSet.SetActive(true);
+                subMenuSet.SetActive(true);
+            }
         }
+
     }
 
 
